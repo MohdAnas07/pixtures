900c10bd681d7fc38768d3911bb3435c620cbac8
MohdAnas07 <anas98144@gmail.com>
2023-02-26 12:59:03 +0530

    loading image fix

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,10 +4,11 @@ import '../styles/loading.scss'
 const Loading = () => {
     return (
         <div className='loading'>
-            <img src="./images/loading.png" alt="" />
+            <img src="../assets/loading.png" alt="" />
             <h3>Loading some awesome Images...</h3>
         </div>
     )
 }
+
 
 export default Loading
